7a1fff9c613b47cce7c551cecbd619dcd71e7ee8
ALB Dev <82672349+prodev2004@users.noreply.github.com>
2022-06-13 22:33:18 +0200

    Add files via upload

---
+++ app.js
@@ -0,0 +1,38 @@
+
+// SUBSCRIBE ==> https://www.youtube.com/albdev
+// SUBSCRIBE ==> https://www.youtube.com/albdev
+
+const buttons = document.querySelectorAll('.btn')
+const textarea = document.querySelector('textarea')
+
+const delete_btn = document.querySelector('.delete')
+const shift_btn = document.querySelector('.shift')
+const space_btn = document.querySelector('.space')
+
+let chars = []
+
+buttons.forEach(btn => {
+    btn.addEventListener('click', () => {
+        textarea.value += btn.innerText
+        chars = textarea.value.split('')
+    })
+})
+
+delete_btn.addEventListener('click', () => {
+    chars.pop()
+    textarea.value = chars.join('')
+})
+
+space_btn.addEventListener('click', () => {
+    chars.push(' ')
+    textarea.value = chars.join('')
+})
+
+shift_btn.addEventListener('click', () => {
+    buttons.forEach(btn => {
+        btn.classList.toggle('upper')
+    })
+})
+
+// SUBSCRIBE ==> https://www.youtube.com/albdev
+// SUBSCRIBE ==> https://www.youtube.com/albdev
